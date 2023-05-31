30f1eca177b919ecc084e125f447a21b4f1ca19e
Matthew Phan <mttphan261@gmail.com>
2023-05-31 07:34:31 -0500

    attempt1 complete

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,17 +4,43 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
+
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, '_name'):
+            self._name = name
+        else:
+            raise Exception
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if new_trip and isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if new_visitor and isinstance(new_visitor, Visitor) and new_visitor not in self._visitors:
+            self._visitors.append(new_visitor)
+            new_visitor.national_parks(self)
+        return self._visitors
     
     def total_visits(self):
-        pass
+        return len(self._trips)
     
     def best_visitor(self):
-        pass
+        # visits = 0
+        # highest_visitor = None
+        # for each in self._visitors:
+        #     times_visited = self._visitors.count(each)
+        #     if times_visited > visits:
+        #         visits = times_visited
+        #         highest_visitor = each
+        # return highest_visitor
+
+        return max(set(self._visitors), key=self._visitors.count)
--- lib/classes/trip.py
+++ lib/classes/trip.py
@@ -1,8 +1,35 @@
 
 class Trip:
+
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+
+        visitor.national_parks(national_park)
+        visitor.trips(self)
+        
+        national_park.visitors(visitor)
+        national_park.trips(self)
+
+        Trip.all.append(self)
+
+
+    def visitor(self, visitor=None):
+        from classes.visitor import Visitor
+        if visitor and isinstance(visitor, Visitor):
+            self.visitor = visitor
+        else:
+            raise Exception
+        return self.visitor
+    
+    def national_park(self, national_park=None):
+        from classes.national_park import NationalPark
+        if national_park and isinstance(national_park, NationalPark):
+            self.national_park = national_park
+        else:
+            raise Exception
+        return self.national_park
